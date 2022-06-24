1951bf0cbbae3469aac59f4e9906165afaae5298
LLSJYY <wpdnjsproo@gmail.com>
2022-06-24 13:21:39 +0900

    newItem destroy 특정 방법

---
+++ studyJs/todolist2/PT/script.js
@@ -0,0 +1,84 @@
+const store = document.querySelector(".store");
+
+/* input Box */
+(function () {
+    // const input = document.querySelector(".new-todo");
+    const input = document.querySelector(".new-todo");
+    const todoList = document.querySelector(".todo-list");
+    const filltersBtn = document.querySelectorAll('.filters a');
+
+    const inputStore = [];
+
+    function createEl(html) {
+        const div = document.createElement('div');
+        div.innerHTML = html
+        return div.firstChild;
+    }
+
+
+    input.addEventListener('keypress', function (e) {
+
+        if (e.keyCode === 13) {
+
+            console.log(input.value);
+
+            var id = Math.floor(Math.random() * 99999999);
+
+            inputStore.push({
+                id,
+                text: input.value,
+                completed: false,
+
+            })
+
+            const newItem = createEl(`<li data-value="${id}"><div><input class="toggle" type ="checkbox"><label>${input.value}</label><button class="destroy"></button></div></li>`)
+            input.value = ""; //value -> innerHTML.
+            
+
+            newItem.querySelector(".destroy").addEventListener('click',e=>{
+                newItem.remove(); // newItem의 destroy를  어떻게 특정하나요 ..? newItem은 여러갠데..
+
+
+
+            })
+
+            todoList.appendChild(newItem); // newItem apeend child;
+            
+        }
+
+      
+
+
+
+        
+    })
+
+    
+
+
+
+    filltersBtn.forEach(function (el, index) {
+        el.addEventListener('click', function (e) {
+            switch (index) {
+                case 0: // All 
+                    break;
+                case 1: // ACTIVE
+                    break;
+                case 2: // COMPLETED
+
+                    todoList.innerHTML = "";
+                    inputStore.forEach(function (data) {
+                        const { id, text } = data;
+                        console.log(data);
+
+                        todoList.innerHTML = ` <li data-value="${id}"  class="completed">${input.value}</li>`
+                    })
+                    break;
+
+            }
+        })
+
+    })
+})()
+
+/* */
