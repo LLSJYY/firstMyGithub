dc92ee1048b34bbbf449e15347780184d5d65fa1
LLSJYY <wpdnjsproo@gmail.com>
2022-06-30 02:07:45 +0900

    filterBtn 중간커밋

--- studyJs/todolist2/todolistDelegate/delegate.js
+++ studyJs/todolist2/todolistDelegate/delegate.js
@@ -2,29 +2,47 @@
 /* 엔터 눌렀을때 delegation 이용 */
 (function () {
 
-  /* keyPress */
+  /* header class */
   const newTodo = document.querySelector(".new-todo");
+  const todoStore = [];
+
   newTodo.addEventListener("keypress", e => {
+    const id = Math.floor(Math.random() * 100000);
+
 
     if (e.keyCode === 13) { //선생님 여기에 공백( "" )을 치면 안넣고싶은데, if문 밖에 없나요 ??
-      todoList.innerHTML += `<li data-id="" class=""><div><input class="toggle" type="checkbox"><label>${newTodo.value}</label><button class="destroy"></button></div></li>`
+
+      todoList.innerHTML += `<li data-id="${id}" class=""><div><input class="toggle" type="checkbox"><label>${newTodo.value}</label><button class="destroy"></button></div></li>`
       console.log(newTodo.value);
+
+      todoStore.push({
+        id, //왜 id : id 값이 바로 들어가나여..?
+        text: newTodo.value,
+        completed: false,
+      })
+      console.log(todoStore);
       newTodo.value = "";
       /** footer todo-count */
-      
+
       if (e.target.tagName == "INPUT") {
-        document.querySelector(".todo-count").innerHTML = `${todoList.childElementCount} ${function () { if (todoList.childElementCount > 1){
-        return "items left" }
-      else if(todoList.childElementCount == 1){
-        return "item left"
-      }}()}`;
-
+        document.querySelector(".todo-count").innerHTML = `${todoList.childElementCount} ${function () {
+          if (todoList.childElementCount > 1) {
+            return "items left"
+          }
+          else if (todoList.childElementCount == 1) {
+            return "item left"
+          }
+        }()}`;
       }
-    }
 
 
+    } // 즉시 바뀌는거.. 제가 생각했을때, keypress/ toggle,destroy 이 3가지에 관련한 이벤트가 일어났을때 todoCount 가 수정되는데,관련 이벤트에 다 todoCount 함수를 만들어 줘야하나요 ? 이러면 좀 답답할거같습니다.
 
   })
+
+
+
+
 
   /* todoList 에 이벤트 걸기 */
   const todoList = document.querySelector(".todo-list");
@@ -41,9 +59,19 @@
     }
   })
 
-  /** footerClass */
+  /** section class */
+
+
+
+
+
+
+
+
+  /** footer class */
+
   const todoCount = document.querySelector(".todo-count");
-  const filters = document.querySelector(".filters");
+  const filtersBtn = document.querySelectorAll(".filters a");
   const btnClear = document.querySelector(".clear-completed");
 
   btnClear.addEventListener('click', function (e) {
@@ -53,11 +81,30 @@
       });
     }
   })
+  /** filterBtn (All,Active,Completed) */
 
-  const todoCountEl = function () {
 
-  }
-
+    filtersBtn.forEach((el,index) => {
+      console.log(el);
+      el.addEventListener("click", function (e) {
+      newTodo.innerHTML = "";
+      switch(index) {
+        case 0: //
+        break;
+        
+        case 1:
+          break;
+        
+        case 2:
+        break;
+        
+        
+        }
+       
+        console.log(e.target)
+        
+      })
+    })
 
 
 
