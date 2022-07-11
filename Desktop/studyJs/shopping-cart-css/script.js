5aafbcdab4ef44106ff4d0d0b332d3e6164dc308
LLSJYY <wpdnjsproo@gmail.com>
2022-07-11 20:19:33 +0900

    new Q3

--- Desktop/studyJs/shopping-cart-css/script.js
+++ Desktop/studyJs/shopping-cart-css/script.js
@@ -7,7 +7,9 @@
 
 
 const productInfoCart = document.querySelectorAll(".flex.justify-between.w-280.p-5");
-const cartItem = `<div class="cart-container">
+
+const storeLocalStorage = function () {
+  const cartItem = `<div class="cart-container">
 <div class="flex gap-15 mt-10">
   <input class="checkbox" name="checkbox" type="checkbox" checked="true">
   <img class="w-144 h-144" src="./assets/images/product.png" alt="PET보틀-정사각(420ml)">
@@ -24,32 +26,54 @@ const cartItem = `<div class="cart-container">
   </div>
   <span class="cart-price">123,456원</span>
 </div>
-</div>` // 상품이 달라지면 이런거는 어떻게 해야하나요 센세?
-
-const storage = function (item) {
-  localStorage.setItem('cart', JSON.stringify(item));
+</div>
+<hr class="divide-line-thin mt-10" />` // 상품이 달라지면 이런거는 어떻게 해야하나요 센세?  newItem 코드번호가 넘긴데 어떻게할까영.. documnet.qs로 잡고싶은데 <hr class="divide-line-thin mt-10" />`안잡힙니다.
+  localStorage.setItem('cart', JSON.stringify(cartItem));
 } 
-
-
+const getStorage =function(){
+  const newDiv = document.querySelector(".cart-container");
+    const getLocalStorage = JSON.parse(localStorage.getItem('cart'));
+    console.log(getLocalStorage)
+    newDiv.innerHTML += `${getLocalStorage}`; 
+    
+    
+    
+  }
+  
 productInfoCart.forEach(function (el) {
+  let count = 0 ;
+  const cartSection = document.querySelector(".cart-left-section");
+ 
   el.addEventListener("click", function (e) {
-    if (e.target.tagName == "IMG") { //선생님 이건 productInfoCart안의 이미지 파일만 선택되는걸까요
+    if (e.target.tagName == "IMG") { //선생님 이건 productInfoCart안의 이미지 파일만 선택되는걸까요 <- 해결.
       console.log("click to img")
-      storage(cartItem); // storage 추가
-
+      count++;
+      storeLocalStorage(); //저장을 3번하고싶은데 안되네요 ..
+     
+      
     }
     if (e.target.classList.contains('product-info__name') || e.target.classList.contains('product-info__price')) {
-      const moveHtml = document.querySelectorAll(".global-nav-button")[2];
-      //debugger
+      
       console.log("상품상세로 이동")
-
+      //redirecting?
+      window.location.href = '/Users/leesu/Desktop/studyJs/shopping-cart-css/orderDetail.html' 
+  
      
     }
   })
 
-  //상품상세로 이동시키고 싶은데 html 불러오는 방법이 뭔지 찾아봤는데 안나옵니다 선생님 뭐라고 검색해야할까요. 
-
+  //상품상세로 이동시키고 싶은데 html 불러오는 방법이 뭔지 찾아봤는데 안나옵니다 선생님 뭐라고 검색해야할까요.  -> redirecting으로 일단 해결.. 
+ 
 })
 
-  
+
+/** CART*/
+
+const clickedCartBtn = document.querySelectorAll(".global-nav-button").forEach(el=>{
+  el.addEventListener('click',e=>{
+    debugger;
+    console.log(e.target); //왜이렇게하면 콘솔에 디버거 걸면 뜨는데 이후에는 사라질까요 ..?
+  })
+
+})
 
